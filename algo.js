2d51bddd6232b9ff1172165433805fdaf9c5a58c
fricskafanni <87277496+fricskafanni@users.noreply.github.com>
2022-05-11 19:18:02 +0200

    Update algo.js

--- algo.js
+++ algo.js
@@ -1,18 +1,58 @@
-import {List, Item} from 'linked-list'
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const list = [];
+export default function App() {
 
-const addItem = (list, newItem) => {
-    if(list.length() === 0) return list.push(newItem);
-    return list.push([...list, newItem]);
-}
+  const [arrayLength, setArrayLength] = useState(4);
+
+  const list = [arrayLength];
+  for (let i=0; i<arrayLength; i++) {
+    list[i] = [];
+  }
+  console.log(list)
+
 
 const addEdge = (v, w) => {
-    addItem(list[v], w);
-    addItem(list[w], v);
+    list[v] = [...list[v], w];
+    list[w] = [...list[w], v];
 }
 
 addEdge(1, 0);
 addEdge(0, 2);
 addEdge(2, 1);
 addEdge(0, 3);
+
+console.log(list)
+
+let paratlan = []
+
+list.map((item, i) => {
+  if(item.length % 2 === 1) {
+    paratlan.push(i)
+  }
+})
+console.log(paratlan)
+
+for(let i = 0; i < paratlan.length; i++){
+  addEdge(paratlan[i], paratlan[i+1])
+  if(!i+2) break;
+
+}
+console.log(list)
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
